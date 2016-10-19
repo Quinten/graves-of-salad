b0e17d8284550b270e47be61d25def7dbd080539
Quinten Clause <quinten.clause@studioemma.eu>
2016-10-19 23:00:06 +0200

    Enemies without pathfinding

--- src/game/states/game.js
+++ src/game/states/game.js
@@ -4,6 +4,7 @@ var gameState = {
     layer: undefined,
     cursors: undefined,
     player: undefined,
+    enemies: undefined,
 
     create: function () {
 
@@ -26,20 +27,35 @@ var gameState = {
         //this.layer.debug = true;
 
         //  Player
-        //this.player = game.add.sprite(48, 48, 'player', 1);
-        this.player = game.add.sprite(48, 48, 'enemy', 1);
+        this.player = game.add.sprite(48, 48, 'player', 1);
+        //this.player = game.add.sprite(48, 48, 'enemy', 1);
         this.player.animations.add('left', [8,9], 10, true);
         this.player.animations.add('right', [1,2], 10, true);
         this.player.animations.add('up', [11,12,13], 10, true);
         this.player.animations.add('down', [4,5,6], 10, true);
 
         game.physics.enable(this.player, Phaser.Physics.ARCADE);
-
         this.player.body.setSize(10, 10, 2, 2);
 
         game.camera.follow(this.player);
 
         this.cursors = game.input.keyboard.createCursorKeys();
+
+        //  Enemies
+        this.enemies = [];
+
+        var enemy = game.add.sprite(2000, 2000, 'enemy', 1);
+        enemy.animations.add('left', [8,9], 10, true);
+        enemy.animations.add('right', [1,2], 10, true);
+        enemy.animations.add('up', [11,12,13], 10, true);
+        enemy.animations.add('down', [4,5,6], 10, true);
+
+        game.physics.enable(enemy, Phaser.Physics.ARCADE);
+        enemy.body.setSize(10, 10, 2, 2);
+
+        this.enemies.push(enemy);
+
+        //console.log(enemy);
 
     },
 
@@ -65,6 +81,33 @@ var gameState = {
             this.player.animations.stop();
         }
 
+        for (var e = 0; e < this.enemies.length; e++) {
+            var enemy = this.enemies[e];
+            game.physics.arcade.collide(enemy, this.layer, this.enemyMapCollide);
+            enemy.body.velocity.set(0);
+
+            if (Math.floor(enemy.body.x / 8) > Math.floor(this.player.body.x / 8)) {
+                enemy.body.velocity.x = -90;
+                enemy.play('left');
+            } else if (Math.floor(enemy.body.x / 8) < Math.floor(this.player.body.x / 8)) {
+                enemy.body.velocity.x = 90;
+                enemy.play('right');
+            } else if (Math.floor(enemy.body.y / 8) > Math.floor(this.player.body.y / 8)) {
+                enemy.body.velocity.y = -90;
+                enemy.play('up');
+            } else if (Math.floor(enemy.body.y / 8) < Math.floor(this.player.body.y / 8)) {
+                enemy.body.velocity.y = 90;
+                enemy.play('down');
+            } else {
+                enemy.animations.stop();
+            }
+
+        }
+
+    },
+
+    enemyMapCollide: function (enemy, map) {
+        console.log('enemy map collide');
     },
 
     resize: function () {
@@ -77,6 +120,7 @@ var gameState = {
         this.layer = undefined;
         this.cursors = undefined;
         this.player = undefined;
+        this.enemies = undefined;
 
     }
 
