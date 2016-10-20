2c4b85418ee32337a969ac89acbcfdb9ca69ebc5
Quinten Clause <quinten.clause@studioemma.eu>
2016-10-20 08:40:31 +0200

    Install a pathfinding library

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         },
         concat: {
             dist: {
-                src: [  "src/lib/phaser.min.js",
+                src: [  "src/lib/**/*.min.js",
                         "src/game/states/**/*.js",
                         "src/game/substates/**/*.js",
                         "src/game/main.js"
